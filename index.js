3ce46e72e9ea258dfc87ec1419cae775f9798cab
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-05 20:10:34 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli = []
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${katzDeliLine[katzDeliLine.length-1]}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length===0){
+    return `There is nobody waiting to be served!`
+  }
+  else{
+    var nextUp = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${nextUp}.`;
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length===0){
+    return`The line is currently empty.`
+  }
+  else{
+    var currentLine = "The line is currently: "
+    for(var i=0;i<katzDeliLine.length;i++){
+      if(i<katzDeliLine.length-1){
+      currentLine += `${i+1}. ${katzDeliLine[i]}, `;
+      }
+      else{
+        currentLine +=`${i+1}. ${katzDeliLine[i]}`;
+      }
+    }
+    return currentLine;
+  }
+}
